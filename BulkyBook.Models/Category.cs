ea7f25b9900f4605cf80f191850c8e3613179c47
Nelson Roza Junior <jr.1403@hotmail.com>
2022-02-10 14:04:44 -0300

    Add project files.

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The category name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The display order is required.")]
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be between 1 and 100 only.")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,133 @@
+﻿using BulkyBook.DataAccess.Context;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (_context.Categories.Any(c => c.Name == category.Name))
+            {
+                ModelState.AddModelError("Name", "The category name already exists.");
+            }
+
+            if (_context.Categories.Any(c => c.DisplayOrder == category.DisplayOrder))
+            {
+                ModelState.AddModelError("DisplayOrder", "The display order already exists.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = $"Category #{category.Id} created successfully";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (!id.HasValue || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id.Value);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (_context.Categories.Any(c => c.Id != category.Id && c.Name == category.Name))
+            {
+                ModelState.AddModelError("Name", "The category name already exists.");
+            }
+
+            if (_context.Categories.Any(c => c.Id != category.Id && c.DisplayOrder == category.DisplayOrder))
+            {
+                ModelState.AddModelError("DisplayOrder", "The display order already exists.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = $"Category #{category.Id} updated successfully";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (!id.HasValue || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id.Value);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = $"Category #{id} deleted successfully";
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
